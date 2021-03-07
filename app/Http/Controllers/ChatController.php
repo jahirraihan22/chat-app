e5bfc1cae14d617dea70c63903bf3f3916c93609
Johir Raihan <jahirraihanmsd@gmail.com>
2021-03-07 11:26:58 +0600

    chat controller  created

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\ChatRoom;
+use App\Models\ChatMessage;
+
+class ChatController extends Controller
+{
+    public function rooms(Request $request){
+        return ChatRoom::al();
+    }
+
+    public function messages(Request $request, $roomId){
+       return ChatRoom::where('chat_roo_id', $roomId)
+        ->with('user')
+        ->orderBy('created_at','DESC')
+        ->get();
+    }
+
+    public function newMessage(Request $request, $roomId){
+        $newMessage = new ChatMessage;
+        $newMessage->user_id = Auth::id();
+        $newMessage->chat_room_id = $roomId;
+        $newMessage->message = $request->message;
+        $newMessage->save();
+        
+        return $newMessage;
+    }
+}
--- app/Models/ChatMessage.php
+++ app/Models/ChatMessage.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class ChatMessage extends Model
 {
     use HasFactory;
+
+    public function room(){
+        return $this->hasOne('App\Models\ChatRoom','id','chat_room_id');
+    }
+
+    public function user(){
+        return $this->hasOne('App\Models\User','id','user_id');
+    }
 }
