2dbc20ad67172a46dac6f4d96883678aba6312f0
Johir Raihan <jahirraihanmsd@gmail.com>
2021-03-07 16:18:27 +0600

    message show done

--- resources/js/Pages/Chat/container.vue
+++ resources/js/Pages/Chat/container.vue
@@ -9,8 +9,13 @@
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <message-container />
-                    <input-message :room='currentRoom'/>
+                    <message-container 
+                        :messages="messages"
+                    />
+                    <input-message 
+                        :room='currentRoom'
+                        v-on:messagesent="getMessages()"
+                    />
                 </div>
             </div>
         </div>
@@ -41,7 +46,6 @@
                 .then( response => {
                     this.chatRooms = response.data;
                     this.setRoom(response.data[0]);
-                    console.log('response.data :>> ', response.data);
                 })
                 .catch (error => {
                     console.log('error : >> ', error);
--- resources/js/Pages/Chat/messageItem.vue
+++ resources/js/Pages/Chat/messageItem.vue
@@ -1,9 +1,11 @@
 <template>
-    <div>Message Item</div>
+    <div>
+        <b>{{ message.user.name }} : </b> {{ message.message }} 
+    </div>
 </template>
 
 <script>
 export default {
-    
+    props : ['message']
 }
 </script>
